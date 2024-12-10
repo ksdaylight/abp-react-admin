148b6dbba4f8b7a27c18c83c0ccfc16b39868e42
ksdaylight <yuanmiwen111@163.com>
2024-12-10 10:18:04 +0800

    wip: add Miwen.Abp.Identity.Session.AspNetCore

---
+++ aspnet-core/modules/identity/Miwen.Abp.Identity.Session.AspNetCore/Miwen/Abp/Identity/Session/AspNetCore/AbpIdentitySessionAspNetCoreModule.cs
@@ -0,0 +1,18 @@
+﻿using Miwen.Abp.IP2Region;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Modularity;
+
+namespace Miwen.Abp.Identity.Session.AspNetCore;
+
+[DependsOn(typeof(AbpAspNetCoreModule))]
+[DependsOn(typeof(AbpIP2RegionModule))]
+[DependsOn(typeof(AbpIdentitySessionModule))]
+public class AbpIdentitySessionAspNetCoreModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.Replace(ServiceDescriptor.Singleton<IIpLocationInfoProvider, IP2RegionLocationInfoProvider>());
+    }
+}
